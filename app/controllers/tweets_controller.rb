927b89fec0a921f99000d0b05caaaa2e0e97d1d5
Diana Badas <diana.badas@gmail.com>
2019-04-17 15:01:14 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -56,7 +56,7 @@ class TweetsController < ApplicationController
       if @tweet && @tweet.username == current_user
         erb :'tweets/edit_tweet'
       else
-        redirect to '/login'
+        redirect to '/tweets'
       end
     else
       redirect to '/login'
